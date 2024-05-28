e0c6824539a60ce4c1e4107c286fe76f29b6e72d
mucahitcetin <mucahitcetin01@gmail.com>
2024-05-28 20:42:20 +0300

    hatalar giderildi

--- src/pages/Protected.jsx
+++ src/pages/Protected.jsx
@@ -8,18 +8,20 @@ const Protected = () => {
 
   useEffect(() => {
     // onAuthStateChanged > kullanıcı oturumundaki değişimi izler
-    onAuthStateChanged(auth, (user) => {
+    const unsub = onAuthStateChanged(auth, (user) => {
       // eğerki kullanıcı varsa yetkisini true'ya çek
       // oturumu kapalıysa yetkisiyi false'a çek
       setIsAuth(user ? true : false);
     });
+
+    return () => unsub();
   }, []);
 
   const navigate = useNavigate();
 
   // eğer yetkisi yoksa kullanıcıyı logine yönlendir
   if (isAuth === false) {
-    return <Navigate to={"/"} />;
+    return <Navigate to={"/"} replace />;
   }
 
   // yetkisi varsa alt route'daki sayfayı göster
