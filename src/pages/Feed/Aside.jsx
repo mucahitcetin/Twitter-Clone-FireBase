9928c8d5809c2dcc406581196e84fe5f42e26e95
mucahitcetin <mucahitcetin01@gmail.com>
2024-05-28 22:53:27 +0300

    hastagler eklendi

--- src/pages/Feed/Aside.jsx
+++ src/pages/Feed/Aside.jsx
@@ -1,4 +1,63 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { CiSearch } from "react-icons/ci";
+import { FiMoreHorizontal } from "react-icons/fi";
+
+const Aside = () => {
+  const hashtags = [
+    "#döner",
+    "#gibi",
+    "#lotr",
+    "#pdr",
+    "#pdrcileryatıyormu?",
+    "#yazılım",
+    "#adanamerkez",
+    "#adananındegisenhavadurumu",
+  ];
+
+  const extractHashtags = (text) => {
+    const hashtags = text.match(/#[\w]+/g);
+    return hashtags ? hashtags.join(" ") : "";
+  };
+
+  return (
+    <div className="max-xl:hidden border border-zinc-600 rounded">
+      <form className="max-w-md mx-auto">
+        <label
+          htmlFor="default-search"
+          className="mb-2 text-sm font-medium text-white sr-only"
+        ></label>
+        <div className="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 text-2xl pointer-events-none">
+            <CiSearch className="w-6 h-6 text-zinc-300" />
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block w-full p-4 ps-14 text-s text-white outline-none rounded-full bg-gray-700 dark:bg-gray-700 placeholder:text-lg"
+            placeholder="Ara"
+          />
+        </div>
+      </form>
+      <div className="m-10 h-full w-[350px] border border-zinc-600 rounded-md p-4">
+        <h2 className="text-lg font-bold mb-4">İlgini çekebilecek gündemler</h2>
+        {hashtags.length > 0 ? (
+          hashtags.map((item, index) => (
+            <div key={index} className="flex justify-between items-center mb-2">
+              <p>{item}</p>
+              <FiMoreHorizontal className="text-zinc-600 cursor-pointer transition hover:scale-150" />
+            </div>
+          ))
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Aside;
+
+/* import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { CiSearch } from "react-icons/ci";
 import { FiMoreHorizontal } from "react-icons/fi";
@@ -89,4 +148,4 @@ const Aside = () => {
   );
 };
 
-export default Aside;
+export default Aside; */
